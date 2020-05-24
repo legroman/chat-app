d691b1aeec48fe80878ac088cbf5482c6c3b8b55
Python-Development <legromant@gmail.com>
2020-05-24 23:22:28 +0300

    reated socket connection, router

--- server/index.js
+++ server/index.js
@@ -4,8 +4,18 @@ const http = require("http");
 
 const port = process.env.PORT || 5000;
 
+const router = require("./router");
+
 const app = express();
 const server = http.createServer(app);
 const io = socket_io(server);
+
+io.on("connection", socket => {
+   console.log("We have a new connection!");
+
+   socket.on("disconnect", () => console.log("User had left!"))
+});
+
+app.use(router);
 
 server.listen(port, () => console.log(`server has been started on port ${port}`));
